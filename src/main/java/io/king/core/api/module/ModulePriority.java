10ef9fb72e46d5fa3fd8f78b2c2c47f6d18b0abd
zkingboos <kingboosoficial@gmail.com>
2020-06-12 22:56:10 -0300

    documentation

--- src/main/java/io/king/core/api/module/ModulePriority.java
+++ src/main/java/io/king/core/api/module/ModulePriority.java
@@ -2,7 +2,6 @@ package io.king.core.api.module;
 
 /**
  * Priority defines the order where module is loaded
- * @Module(ModulePriority.SYSTEM) > @Module(ModulePriority.NORMAL)
  * System is be loaded first of normal
  */
 public enum ModulePriority {
